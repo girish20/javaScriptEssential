650465be17849bf2f18caa24d1c34cb6af7663c9
girish.bhopale@gmail.com <girish.bhopale@gmail.com>
2025-03-03 02:58:19 -0500

    message

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -42,3 +42,9 @@ switch (userType) {
 }
 
 console.log("User Category:", userCategory);
+
+let isAuthenticated = true;
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
+
+console.log("Authentication Status:", authenticationStatus);
